03bb53f6595c13e7719be1565bd91f9c56a93571
Ruthe-Nobrega <ruthe.nobrega@gmail.com>
2025-05-30 18:10:30 -0300

    adicionei html e css

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Home
-
+from .views import Home, uph_view, ump_view, saf_view, ucp_view, upa_view
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('uph/', uph_view, name='uph'),
+    path('ump/', ump_view, name='ump'),
+    path('saf/', saf_view, name='saf'),
+    path('ucp/', ucp_view, name='ucp'),
+    path('upa/', upa_view, name='upa'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -5,3 +5,18 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+
+def uph_view(request):
+    return render(request, 'uph.html')
+
+def ump_view(request):
+    return render(request, 'ump.html')
+
+def saf_view(request):
+    return render(request, 'saf.html')
+
+def ucp_view(request):
+    return render(request, 'ucp.html')
+
+def upa_view(request):
+    return render(request, 'upa.html')
